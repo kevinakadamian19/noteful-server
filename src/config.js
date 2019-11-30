8c7080dd3ba4d8ecc3f9b34a75dfcce4873c198f
Kevin Damian Ta <dudeitsdamian@Kevins-MacBook.local>
2019-11-30 14:44:56 -0800

    Adding ORIGIN for CORS

--- src/config.js
+++ src/config.js
@@ -2,5 +2,6 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/blogful',
-    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/blogful-test'
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/blogful-test',
+    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "http://localhost:3000/"
 }
